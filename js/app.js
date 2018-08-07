1ffa43fb85b9a887bde58f5217a98a2e8a868777
dlchambersjr <dlchambersjr@gmail.com>
2018-08-07 14:58:53 -0700

    save before attempting change of previous commit description

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 'use strict';
 
-alert('IT IS ALIVE');
+// Get the user's name
+var userName = prompt('Hi there, what is your name?');
+
+alert('Thank you for visiting my \"About Me\" page. I\'m going to ask you some questions about me.  THese are all yes/no questions, so feel free to answer with y/n or yes/no.');
+
+// Ask them five questions
+
+var answerOne = prompt('Do I have any pets?').toLowerCase();
+
+if (answerOne === 'y' || answerOne === 'yes') {
+    alert('You got it right');
+    } else {
+        alert('You got it wrong');
+    };

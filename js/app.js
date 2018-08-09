5df182b3ec528b6160f1223d9b9b46867514de6d
Jeff Borda <jeffborda@outlook.com>
2018-08-09 13:25:36 -0700

    Functions thru 3

--- js/app.js
+++ js/app.js
@@ -10,41 +10,56 @@ var correctQuestions = 0;
 
 alert('Hello ' + userName + ', thank you for visiting my "About Me" page. Before you see the page, I\'m going to ask you some questions to see how much you know about me already. These are all yes/no questions, so feel free to answer with y/n or yes/no.');
 
-// Question #1
-var answerOne = prompt('Question #1, ' + userName + ': Do I have two dogs, a cat, and a pet fish named "Gas Station?"').toLowerCase();
 
-if (answerOne === 'n' || answerOne === 'no') {
-  alert('You got it right, ' + userName +'!  I only have two dogs now.  Their names are Mia and Henderson.  They are sibling Black Lab - Golden Retrievers');
-  correctQuestions ++;
-  console.log('user was asked about my pets and respnded with ' + answerOne + '. They currently have ' + correctQuestions + 'questions correct.');
-} else {
-  alert('I\'m sorry, ' + userName + '. I used to have all of those animals, but now I have just two dogs.');
-  console.log('user was asked about my pets and responded with ' + answerOne + '. They currently have ' + correctQuestions + ' questions correct.');
+// Define Question Functions
+
+function questionOne() {
+  var answerOne = prompt('Question #1, ' + userName + ': Do I have two dogs, a cat, and a pet fish named "Gas Station?"').toLowerCase();
+
+  if (answerOne === 'n' || answerOne === 'no') {
+    alert('You got it right, ' + userName +'!  I only have two dogs now.  Their names are Mia and Henderson.  They are sibling Black Lab - Golden Retrievers');
+    correctQuestions ++;
+    console.log('user was asked about my pets and respnded with ' + answerOne + '. They currently have ' + correctQuestions + 'questions correct.');
+  } else {
+    alert('I\'m sorry, ' + userName + '. I used to have all of those animals, but now I have just two dogs.');
+    console.log('user was asked about my pets and responded with ' + answerOne + '. They currently have ' + correctQuestions + ' questions correct.');
+  }
 }
+
+function questionTwo() {
+  var answerTwo = prompt('Question #2, ' + userName + ': Did I once live in the country of Turkey').toLowerCase();
+
+  if (answerTwo === 'y' || answerTwo === 'yes') {
+    alert('You\'re so smart, ' + userName + '! I lived in the country of Turkey when I was in the second and third grade.  It\'s one of my favorite memories.');
+    correctQuestions ++;
+    console.log('the user was asked about living in Turkey and they answered ' + answerTwo + '. They now have ' + correctQuestions + ' correct');
+  } else {
+    alert('Bummer, ' + userName + '. You missed that one.  I did live in Turkey when I was in the second and third grade.  It was one of my favorite places.  Better luck on the next question.');
+    console.log('the user was asked about living in Turkey and they answered ' + answerTwo + '. They now have ' + correctQuestions + ' questions correct');
+  }
+}
+
+function questionThree(){
+  var answerThree = prompt('OK, ' + userName +'... here is Question #3: Have I been married for thirty years?').toLowerCase();
+
+  if (answerThree === 'y' || answerThree === 'yes') {
+    alert('Nailed it, ' + userName + '! I married my amazing wife, Lisa, when I was 18 and we just celebrated our 30th anniversary last April.');
+    correctQuestions ++;
+    console.log('the user was asked about how long I have been married and answered ' + answerThree + '. They now have ' + correctQuestions + ' correct');
+  } else {
+    alert('You missed that one, ' + userName + '. I have been married for 30 years, but it feels like I got married yesterday.');
+    console.log('the user was asked about how long I have been married and answered ' + answerThree + '. They now have ' + correctQuestions + ' questions correct');
+  }
+}
+
+// Question #1
+questionOne();
 
 // Question #2
-var answerTwo = prompt('Question #2, ' + userName + ': Did I once live in the country of Turkey').toLowerCase();
-
-if (answerTwo === 'y' || answerTwo === 'yes') {
-  alert('You\'re so smart, ' + userName + '! I lived in the country of Turkey when I was in the second and third grade.  It\'s one of my favorite memories.');
-  correctQuestions ++;
-  console.log('the user was asked about living in Turkey and they answered ' + answerTwo + '. They now have ' + correctQuestions + ' correct');
-} else {
-  alert('Bummer, ' + userName + '. You missed that one.  I did live in Turkey when I was in the second and third grade.  It was one of my favorite places.  Better luck on the next question.');
-  console.log('the user was asked about living in Turkey and they answered ' + answerTwo + '. They now have ' + correctQuestions + ' questions correct');
-}
+questionTwo();
 
 // Question #3
-var answerThree = prompt('OK, ' + userName +'... here is Question #3: Have I been married for thirty years?').toLowerCase();
-
-if (answerThree === 'y' || answerThree === 'yes') {
-  alert('Nailed it, ' + userName + '! I married my amazing wife, Lisa, when I was 18 and we just celebrated our 30th anniversary last April.');
-  correctQuestions ++;
-  console.log('the user was asked about how long I have been married and answered ' + answerThree + '. They now have ' + correctQuestions + ' correct');
-} else {
-  alert('You missed that one, ' + userName + '. I have been married for 30 years, but it feels like I got married yesterday.');
-  console.log('the user was asked about how long I have been married and answered ' + answerThree + '. They now have ' + correctQuestions + ' questions correct');
-}
+questionThree();
 
 // Question #4
 var answerFour = prompt('Almost done.  Here is Question #4, ' + userName + ': Do I have four children?').toLowerCase();
